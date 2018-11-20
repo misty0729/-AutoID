1050a7eecde29c1a9bebd4fb1c2e1cedeb3594ad
qianzgit <qianz_need@163.com>
2018-11-20 15:20:54 +0800

    上传了参考文献

--- svm.py
+++ svm.py
@@ -3,7 +3,7 @@ from sklearn import svm
 from sklearn import datasets
 from sklearn.model_selection import train_test_split as ts
 
-#import our data
+# import our data
 iris = datasets.load_iris()
 X = iris.data
 print("mydata:")
@@ -12,28 +12,28 @@ y = iris.target
 print("mytarget")
 print(y)
 
-#split the data to  7:3
-X_train,X_test,y_train,y_test = ts(X,y,test_size=0.3)
+# split the data to  7:3
+X_train, X_test, y_train, y_test = ts(X, y, test_size=0.3)
 
 # select different type of kernel function and compare the score
 
 # kernel = 'rbf'
-clf_rbf = svm.SVC(kernel='rbf',gamma='scale')
+clf_rbf = svm.SVC(kernel='rbf', gamma='scale')
 print(clf_rbf)
-clf_rbf.fit(X_train,y_train)
-score_rbf = clf_rbf.score(X_test,y_test)
-print("The score of rbf is : %f"%score_rbf)
+clf_rbf.fit(X_train, y_train)
+score_rbf = clf_rbf.score(X_test, y_test)
+print("The score of rbf is : %f" % score_rbf)
 
 # kernel = 'linear'
-clf_linear = svm.SVC(kernel='linear',gamma='scale')
+clf_linear = svm.SVC(kernel='linear', gamma='scale')
 print(clf_linear)
-clf_linear.fit(X_train,y_train)
-score_linear = clf_linear.score(X_test,y_test)
-print("The score of linear is : %f"%score_linear)
+clf_linear.fit(X_train, y_train)
+score_linear = clf_linear.score(X_test, y_test)
+print("The score of linear is : %f" % score_linear)
 
 # kernel = 'poly'
-clf_poly = svm.SVC(kernel='poly',gamma='scale')
+clf_poly = svm.SVC(kernel='poly', gamma='scale')
 print(clf_poly)
-clf_poly.fit(X_train,y_train)
-score_poly = clf_poly.score(X_test,y_test)
-print("The score of poly is : %f"%score_poly)
+clf_poly.fit(X_train, y_train)
+score_poly = clf_poly.score(X_test, y_test)
+print("The score of poly is : %f" % score_poly)
